abf357dbdf9fb78a01aca668232096726be08067
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2025-02-22 02:00:47 +0300

    update dark theme palette

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -1,6 +1,7 @@
 import {
-  type OriginalPalette,
-  type OriginalPaletteOptions,
+  type Palette as OriginalPalette,
+  type PaletteOptions as OriginalPaletteOptions,
+  type SimplePaletteColorOptions as OriginalSimplePaletteColorOptions,
 } from "@mui/material/styles";
 
 declare module "@mui/material/styles" {
@@ -9,5 +10,12 @@ declare module "@mui/material/styles" {
   }
   interface Palette extends OriginalPalette {
     transparent: { selection: string };
+  }
+  interface SimplePaletteColorOptions extends OriginalSimplePaletteColorOptions {
+    main: string;
+    dark: string;
+    light: string;
+    contrastText: string;
+    background: string;
   }
 }
