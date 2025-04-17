18950d5cc953c7e5fbadd3304673e1297cff442e
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2025-04-18 00:00:43 +0300

    Add curvy border to tabs

--- src/views/pages/Beneficiary/components/BeneficiaryTabs/index.tsx
+++ src/views/pages/Beneficiary/components/BeneficiaryTabs/index.tsx
@@ -1,11 +1,13 @@
 import { styled } from "@mui/material/styles";
 import Tab, { tabClasses } from "@mui/material/Tab";
 import Tabs, { tabsClasses } from "@mui/material/Tabs";
+import { varAlpha } from "src/themes/styles";
 
 const TabItem = styled(Tab)(({ theme }) =>
   theme.unstable_sx({
     overflow: "initial",
-    px: 2,
+    px: 1,
+    mx: 0.5,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     color: theme.palette.text.primary,
@@ -18,16 +20,34 @@ const TabItem = styled(Tab)(({ theme }) =>
       minWidth: "160px",
     },
     "&:hover": {
-      [`&:not(.${tabClasses.selected})`]: {
-        backgroundColor: "rgba(0 0 0 / 0.2)",
-      },
+      [`&:not(.${tabClasses.selected}), &:not(.${tabClasses.selected}):before, &:not(.${tabClasses.selected}):after`]:
+        {
+          backgroundColor: varAlpha(theme.palette.grey["500Channel"], 0.2),
+        },
     },
-    [`&.${tabClasses.selected}`]: {
-      backgroundColor:
-        theme.palette.mode === "dark"
-          ? theme.palette.grey[800]
-          : theme.palette.grey[300],
-      color: theme.palette.text.primary,
+    [`&.${tabClasses.selected}, &.${tabClasses.selected}:after, &.${tabClasses.selected}:before`]:
+      {
+        backgroundColor:
+          theme.palette.mode === "dark"
+            ? theme.palette.grey[800]
+            : theme.palette.grey[300],
+        color: theme.palette.text.primary,
+      },
+    "&:before, &:after": {
+      position: "absolute",
+      bottom: 0,
+      width: 10,
+      height: 10,
+      content: '" "',
+      transition: "0.2s",
+    },
+    [`&:before`]: {
+      left: -10,
+      clipPath: "path('M 10 10 C 2 10 0 8 0 0 L 0 10 Z')",
+    },
+    [`&:after`]: {
+      right: -10,
+      clipPath: "path('M 0 10 C 8 10 10 8 10 0 L 10 10 Z')",
     },
   })
 );
@@ -53,7 +73,7 @@ export function BeneficiaryTabs({ tabs, currentTab, setCurrentTab }: Props) {
       }}
     >
       {tabs.map((tab) => (
-        <TabItem key={tab.name} label={tab.label} />
+        <TabItem key={tab.name} label={tab.label} disableRipple />
       ))}
     </Tabs>
   );
